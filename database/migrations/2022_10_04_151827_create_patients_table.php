4ec31fdad6ebbb4faa476b2bd729b535295be4d3
Franssen <ngoiefranssen04@gmail.com>
2022-10-04 17:52:32 +0200

    Wip

--- database/migrations/2022_10_04_151827_create_patients_table.php
+++ database/migrations/2022_10_04_151827_create_patients_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('')->constrained()->onDelete('CASCADE');
             $table->timestamps();
         });
     }
