244e1c9643560d1c5af79e0f11a69b8da18f333b
Franssen <ngoiefranssen04@gmail.com>
2022-10-05 09:44:03 +0200

    Wip

--- database/migrations/2022_10_04_151959_create_medications_table.php
+++ database/migrations/2022_10_04_151959_create_medications_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('medications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pharmacist_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('category_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('invoice_id')->constrained()->onDelete('CASCADE');
+            $table->string('name_medication');
+            $table->date('manufacturing_date');
+            $table->date('Expiry_date');
+            $table->string('description_medication');
             $table->timestamps();
         });
     }
