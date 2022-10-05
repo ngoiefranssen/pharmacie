2478e4b7970af2433acacb6ceddd3908b6eb1a25
Franssen <ngoiefranssen04@gmail.com>
2022-10-05 09:30:35 +0200

    Wip

--- database/migrations/2022_10_04_151941_create_categories_table.php
+++ database/migrations/2022_10_04_151941_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pharmacist_id')->constrained()->onDelete('CASCADE');
+            $table->string('name_category');
+            $table->string('description_category');
             $table->timestamps();
         });
     }
