df2f689456e7563c9ebaa049b1bed1ed5f29ad28
Franssen <ngoiefranssen04@gmail.com>
2022-10-13 14:45:23 +0200

    Wip

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('home.main'); });
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
+])->group(function () { Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
