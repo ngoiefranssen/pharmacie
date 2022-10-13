4d29485c008f170c2a67f277ef8e702736eb4e0e
Franssen <ngoiefranssen04@gmail.com>
2022-10-13 14:45:46 +0200

    Wip

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,7 @@ Route::get('/', function () { return view('home.main'); });
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'
 ])->group(function () { Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+        return view('dashboard'); })->name('dashboard');
 });
 
 Route::resource('cashiers', CashierController::class);
