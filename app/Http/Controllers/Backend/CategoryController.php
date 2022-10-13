0e567afc9a3b96ab5e87995f944f9445b1d4e8b9
Franssen <ngoiefranssen04@gmail.com>
2022-10-13 13:13:04 +0200

    Wip

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -16,8 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::select('pharmacist_id', 'name_category',
-                                    'description_category')->get_browser();
+        $categories = Category::get_browser();
 
         return view('categories.index', compact('categories'));
     }
