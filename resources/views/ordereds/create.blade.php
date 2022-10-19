f3abad79e9be26b0563aeb40a565a00fbe56bdbb
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 13:57:29 +0200

    Wip

--- resources/views/ordereds/create.blade.php
+++ resources/views/ordereds/create.blade.php
@@ -2,15 +2,17 @@
     
 <div class="">
     <div class="md:grid md:grid-cols-0 md:gap-6">
-        <p class="text-sm leading-center"></p>
+        <p class="te                <h1>Ajouter un patient</h1>
+            xt-sm leading-center"></p>
       <div class="mt-5 md:mt-0 md:col-span-2  pt-14">
         <form action="{{ route('ordereds.store') }}" method="POST" class="" enctype="multipart/form-data">
             @csrf
             @method('post')
           <div class="shadow overflow-hidden sm:rounded-md">
             <div class="px-4 py-5 bg-white sm:p-6">
-              <div class="grid grid-cols-6 gap-6">
+                <h1 class="text-center">Commande</h1>
 
+              <div class="grid grid-cols-6 gap-6">
                 <div class="col-span-6 sm:col-span-3">
                     @error('patient_id')
                          <div class="text-blue-600">{{ $message }}</div>
@@ -61,7 +63,7 @@
 
             </div>
             </div>
-            <div class="px-2 py-3 bg-gray-50 text-right sm:px-6">
+            <div class="px-2 py-3 bg-gray-50 text-right sm:px-3">
               <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="submit">
                 Save
               </button>
