363d78a59b31992171725178f72ef8b11c895755
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 15:03:23 +0200

    Wip

--- resources/views/ordereds/create.blade.php
+++ resources/views/ordereds/create.blade.php
@@ -57,7 +57,7 @@
                     @enderror
                     <div class="mb-4 xl:w-100">
                         <textarea class=" form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
-                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="ordered_description" name="ordered_description" rows="3" placeholder="Your description...."></textarea>
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="ordered_description" name="ordered_description" rows="3" placeholder="Entrer la description...."></textarea>
                     </div>
                 </div>
 
