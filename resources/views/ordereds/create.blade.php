459369125ce79fe7ee76fd878b4591fe12e6430f
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 13:50:48 +0200

    Wip

--- resources/views/ordereds/create.blade.php
+++ resources/views/ordereds/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     
-<div class=" sm:mt-2 flex justify-center">
+<div class="">
     <div class="md:grid md:grid-cols-0 md:gap-6">
         <p class="text-sm leading-center"></p>
       <div class="mt-5 md:mt-0 md:col-span-2  pt-14">
@@ -61,7 +61,7 @@
 
             </div>
             </div>
-            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <div class="px-2 py-3 bg-gray-50 text-right sm:px-6">
               <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="submit">
                 Save
               </button>
