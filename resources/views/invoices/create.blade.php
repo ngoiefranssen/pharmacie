fb84a0982981717a8f4614988f246323577525bc
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 14:43:39 +0200

    Wip

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     
-<div class="flex flex-justify-center">
+<div class="flex content-center">
     <div class="">
         <p class="text-sm leading-center"></p>
       <div class="pt-14">
@@ -14,7 +14,7 @@
                 </ul>
             </div>
           @endif --}}
-        <form class="flex flex-justify-center" action="{{ route('invoices.store') }}" method="POST" class="" enctype="multipart/form-data">
+        <form action="{{ route('invoices.store') }}" method="POST" class="" enctype="multipart/form-data">
             @csrf
             @method('post')
           <div class="shadow overflow-hidden sm:rounded-md">
