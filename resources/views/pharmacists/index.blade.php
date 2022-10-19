4a00287a27ece57cf9b265b7018078cb4d34ce1d
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 14:09:08 +0200

    Wip

--- resources/views/pharmacists/index.blade.php
+++ resources/views/pharmacists/index.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
 
 
-<div class="my-5 ml-12 flex justify-left ">
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Pharmaciens') }}
+    </h2>
+  </x-slot>
+
+<div class="my-5 ml-12">
     <a href="{{ route('pharmacists.create') }}" class="flex flex-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded my-3 ml-5">Add_pharmacist</a>
 </div>
 
