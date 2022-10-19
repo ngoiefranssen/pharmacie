b7289831ac95d96fc9af92c8534e29aac73d0b27
Franssen <ngoiefranssen04@gmail.com>
2022-10-19 13:49:25 +0200

    Wip

--- resources/views/ordereds/index.blade.php
+++ resources/views/ordereds/index.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout>
 
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Commandes') }}
+    </h2>
+  </x-slot>
+
 <div class="my-5 ml-12">
     <a href="{{ route('ordereds.create') }}" class="flex flex-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded my-3 ml-5">Add_ordered</a>
 </div>
